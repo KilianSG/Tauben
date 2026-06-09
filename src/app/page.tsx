c116811bceb7ff92940f79dbb84d8639a9da3948
xiata <xiata@tu-berlin.de>
2026-06-09 09:45:39 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import NinasWorkRootPage from "@/components/ninas-work/NinasWorkRootPage";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
     return <NinasWorkRootPage />;
 }
